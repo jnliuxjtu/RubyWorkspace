a88761d4520e9365407ae2c1e4d387eff920429b
jnliuxjtu <jnliuxjtu@qq.com>
2016-12-17 19:50:40 +0800

    Test 1

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(n)
+    ans = Array.new
+    for i in 1..n
+        if i%3==0 and i%5==0
+            ans<<"FizzBuzz"
+        elsif i%3==0
+            ans<<"Fizz"
+        elsif i%5==0
+            ans<<"Buzz"
+        else
+            ans<<i.to_s
+        end
+    end
+    return ans
+end
---
+++ hello.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(n)
+    ans = Array.new
+    for i in 1..n
+        if i%3==0 and i%5==0
+            ans<<"FizzBuzz"
+        elsif i%3==0
+            ans<<"Fizz"
+        elsif i%5==0
+            ans<<"Buzz"
+        else
+            ans<<i.to_s
+        end
+    end
+    return ans
+end
+puts "Hello World!\n"
+puts fizzbuzz(15)
